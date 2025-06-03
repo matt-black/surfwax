06edc4d658472576910a1f93df837fa6a3a33f0e
Matt Black <matt.black7@gmail.com>
2025-06-03 16:32:29 -0400

    sarinas 2nd commit

--- src/surfwax/haar.py
+++ src/surfwax/haar.py
@@ -3,7 +3,10 @@ import jax.numpy as jnp
 from jax.tree_util import Partial
 from jaxtyping import Array, Float
 
-from .boxfilter import haarx2, haary2
+from .boxfilter import haarx2, haarx3d, haary2, haary3d, haarz3d
+
+# import haar x3
+from .types import IntegralVolume  # not accessed in Pylance
 from .types import IntegralImage
 
 
@@ -30,3 +33,34 @@ def haar_response_2d(
         im.shape
     )
     return jnp.stack([x, y], axis=0) / (filt_size**2)
+
+
+def haar_response_3d(
+    vol: IntegralVolume, filt_size: int
+) -> Float[Array, "2 d h w"]:
+    """haar_response Compute the responses to a 3D Haar wavelet of specified size for the input volume.
+
+    Args:
+        vol (IntegralVolume): input volume.
+        filt_size (int): size of the Haar filter (in the filtering direction).
+
+    Returns:
+        Float[Array, "3 d h w"]: dx, dy, dx responses at each voxel
+    """
+
+    coords = jnp.stack(
+        jnp.meshgrid(*[jnp.arange(0, s) for s in vol.shape], indexing="ij"),
+        axis=-1,
+    ).reshape(
+        -1, 3
+    )  # [num_voxels, 3]
+    dx = (jax.vmap(Partial(haarx3d, vol, filt_size), 0, 0)(coords)).reshape(
+        vol.shape
+    )
+    dy = (jax.vmap(Partial(haary3d, vol, filt_size), 0, 0)(coords)).reshape(
+        vol.shape
+    )
+    dz = (jax.vmap(Partial(haarz3d, vol, filt_size), 0, 0)(coords)).reshape(
+        vol.shape
+    )
+    return jnp.stack([dx, dy, dz], axis=0) / (filt_size**3)
